5f16cac2fe377636a54d29c1ced267c2e81e7937
Andres Felipe Gomez Herrera <agomezh@sura.com.co>
2024-11-26 15:34:03 -0500

    Continuacion con el home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,14 +34,49 @@ function Home() {
           <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-0"></div>
         </div>
 
-        <div id="about" className="container mx-auto p-4 mt-10">
-          <h2 className="text-center text-3xl font-bold">Acerca de mí</h2>
-          <p className="text-center mt-4">Soy un desarrollador apasionado por el aprendizaje y los desafíos.</p>
+        <div id="about" className="relative h-screen flex justify-center items-center">
+            {/* Video de fondo */}
+            <video
+            className="absolute top-0 left-0 w-full h-full object-cover"
+            autoPlay
+            loop
+            muted
+            >
+            <source src="/videos/gamer.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          {/* Contenido superpuesto */}
+          <div className="relative z-10 text-center text-white p-4">
+            <h1 className="text-4xl font-bold">Acerca de mí</h1>
+            <p className="mt-4">
+              Soy un desarrollador apasionado por el aprendizaje y los desafíos.
+            </p>
+            {/* Filtro oscuro opcional */}
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-0"></div>
+          </div>
         </div>
 
-        <div id="projects" className="container mx-auto p-4 mt-10">
-          <h2 className="text-center text-3xl font-bold">Proyectos</h2>
-          <p className="text-center mt-4">Estos son algunos de mis proyectos recientes.</p>
+        <div id="projects" className="relative h-screen flex justify-center items-center">
+            {/* Video de fondo */}
+            <video
+            className="absolute top-0 left-0 w-full h-full object-cover"
+            autoPlay
+            loop
+            muted
+            >
+            <source src="/videos/Proyectos.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          {/* Contenido superpuesto */}
+          <div className="relative z-10 text-center text-white p-4">
+            <h1 className="text-4xl font-bold">Proyectos!</h1>
+            <p className="mt-4">
+              Estos son algunos de mis proyectos recientes.
+            </p>
+            {/* Filtro oscuro opcional */}
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-0"></div>
+          </div>
+
         </div>
 
         <div id="contact" className="container mx-auto p-4 mt-10">
